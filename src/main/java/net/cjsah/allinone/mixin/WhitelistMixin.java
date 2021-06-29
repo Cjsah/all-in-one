148317ca6578a533e8d3ea4ad21dd662e42fc701
Cjsah <2684117397@qq.com>
2021-06-29 13:53:48 +0800

    0.4

---
+++ src/main/java/net/cjsah/allinone/mixin/WhitelistMixin.java
@@ -0,0 +1,18 @@
+package net.cjsah.allinone.mixin;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.server.Whitelist;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(Whitelist.class)
+public class WhitelistMixin {
+	/**
+	 * @author Cjsah
+	 * @reason 使白名单匹配名字而不再匹配uuid
+	 */
+	@Overwrite
+	public String toString(GameProfile gameProfile) {
+		return gameProfile.getName();
+	}
+}
